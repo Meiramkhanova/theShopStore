57fbb32de7d55d33975853ede501e4a59c1e5710
Adina <adinaone04@gmail.com>
2023-11-09 16:02:47 +0600

    first commit

---
+++ js/data.js
@@ -0,0 +1,58 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Доводы Рассудка",
+    price: 1890,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "/assets/img1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Чувство",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "/assets/img2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Три Товарища",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "/assets/img3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Маленький Принц",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "/assets/img4.jpg",
+  },
+  {
+    id: "thiecbawdjksadjk",
+    name: "Маленькие Женщины",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "/assets/img5.jpg",
+  },
+  {
+    id: "iuertrywebncdjksadjk",
+    name: "Гордость и Предубеждение",
+    price: 200,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "/assets/img6.jpeg",
+  },
+  {
+    id: "thierytbvcbvzdhadjk",
+    name: "Прощай, Оружие!",
+    price: 450,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "/assets/img7.jpg",
+  },
+  {
+    id: "trfoiwfcnbcawdjksadjk",
+    name: "Таинственный Сад",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "/assets/img8.jpg",
+  },
+];
